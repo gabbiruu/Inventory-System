079b05b3a7bad56268ed1a2a1388ce8786098f98
gabbiruu <136967621+gabbiruu@users.noreply.github.com>
2023-06-18 19:10:29 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,49 @@
-
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     static Scanner sc = new Scanner(System.in);
-
 
     public static void main(String[] args) {
         MainDisplay();
     }
 
-
-    public static void MainDisplay(){
+    public static void MainDisplay() {
         char choice;
         System.out.println("||==============================||");
         System.out.println("||        E-GroceMarket!        ||");
         System.out.println("||==============================||");
         System.out.println("||                              ||");
+        System.out.println("||                              ||");
         System.out.println("||        A. Admin              ||");
         System.out.println("||        B. Cashier            ||");
         System.out.println("||        C. Exit               ||");
         System.out.println("||                              ||");
+        System.out.println("||                              ||");
         System.out.println("||==============================||");
         System.out.print("Enter your choice: ");
         choice = sc.next().toUpperCase().charAt(0);
-        System.out.println("");
 
-
-        switch (choice){
-            case 'A':
+        switch (choice) {
+            case 'A' -> {
                 Admin admin = new Admin();
-                admin.displayAdmin();
-                break;
-            case 'B':
+                admin.adminLogin();
+            }
+            case 'B' -> {
                 Cashier cashier = new Cashier();
-                cashier.displayCashier();
-                break;
-            case 'C':
-                System.exit(0);
-                System.out.println("Thank you for visiting!!");
-                break;
+                cashier.cashierLogin();
+            }
+            case 'C' -> {
+                System.out.println("");
+                System.out.println("Goodbye and thank you.");
+                System.exit(0); // Terminate the program
+            }
+            default -> {
+                System.out.println("Invalid choice. Please try again.");
+                MainDisplay(); // Recursively call MainDisplay() for a valid choice
+            }
         }
     }
+}
